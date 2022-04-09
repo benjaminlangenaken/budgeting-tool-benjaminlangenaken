2b815955e53567e5f831f98a1c59b57f07f7997a
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-04-09 18:09:20 +0200

    Add increments to id's

--- database/migrations/2022_04_04_094410_create_transactions_table.php
+++ database/migrations/2022_04_04_094410_create_transactions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('transactions', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->foreignId('user_id');
             $table->foreignId('account_id');
             $table->string('description');
--- database/migrations/2022_04_04_100342_create_accounts_table.php
+++ database/migrations/2022_04_04_100342_create_accounts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('accounts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->foreignId('user_id');
             $table->string('IBAN');
             $table->decimal('balance', 10, 2);
