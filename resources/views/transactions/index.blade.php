b6b0a1d5e1317de9fcc14e3a820782d321385e47
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-04-09 19:26:39 +0200

    Small styling adjustments

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -2,20 +2,20 @@
 
     @include('_partials.header')
 
-    <main class="max-w-6xl mx-auto mt-6 lg:mt-10 space-y-6">
+    @if ($transactions->count() > 0)
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-10 space-y-6 mb-auto">
 
-        <h1 class="text-3xl font-bold">Remaining budget=
-            {{ number_format($income[0]->sum - $expenses[0]->sum, 2, ',', '.') . ' ' . 'EUR' }}
-        </h1>
+            <h1 class="text-3xl font-bold">Remaining budget=
+                {{ number_format($income[0]->sum - $expenses[0]->sum, 2, ',', '.') . ' ' . 'EUR' }}
+            </h1>
 
-        @if ($transactions->count() > 0)
             <x-categories-grid :categories="$categories"/>
-
-        @else
-            <p class="text-center">Please add some transactions to create your budget...</p>
-        @endif
-
-    </main>
+        </main>
+    @else
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-10 space-y-6 mb-auto flex h-screen items-center">
+                <p class="text-center ">Please add some transactions to create your budget...</p>
+        </main>
+    @endif
 
     @include('_partials.footer')
 
